ede73c6b0cc4109f479ab7853f51ac6d662f0476
melisademirparmak <melisademirparmaak@gmail.com>
2022-01-13 18:18:45 +0300

    add: connect a  wallet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Home from './pages/home';
+import { Web3ReactProvider } from '@web3-react/core';
+import Web3 from 'web3';
+
+const getLibrary = (provider) => {
+  return new Web3(provider);
+};
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div className="App">
+        <Home />
+      </div>
+    </Web3ReactProvider>
   );
 }
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useWeb3React } from '@web3-react/core';
+import { injected } from '../../components/connectors/injected';
 
 const Home = () => {
+  const { active, account, activate, deactive } = useWeb3React();
+
+  const connect = async () => {
+    try {
+      await activate(injected);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
   return (
     <div>
-      <button>Connect to Wallet</button>
+      <button onClick={connect}>Connect to Wallet</button>
     </div>
   );
 };
